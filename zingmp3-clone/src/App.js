3e69ce97c592ae1f6b7361164640765e9c04c864
kimthaohuynhthi <kimthao.huynhthi@m.citynow.vn>
2021-11-04 00:49:53 +0700

    setting stylelint

--- zingmp3-clone/src/App.js
+++ zingmp3-clone/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save 12 todsdsdsdsdsdsd reload.
+          Edit <code>src/App.js</code> and save 12 tosdsđsddsdsdsdsdsdsd reload.
         </p>
         <a
           className="App-link"

b905866f696c9977f495ae93fe5ec8a06d825cb4
kimthaohuynhthi <kimthao.huynhthi@m.citynow.vn>
2021-11-08 17:00:59 +0700

    delete comment NewVietnameseSong/style.scss

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -4,18 +4,18 @@ import React from "react";
 import Header from "./Header";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-// import CooperationPartner from './CooperationPartner'
-// import HotAlbum from './HotAlbum'
-// import HotArtist from './HotArtist'
-// import HotTopic from './HotTopic'
-// import HotVideo from './HotVideo'
-// import MusicForYou from './MusicForYou'
-// import MusicNews from './MusicNews'
-// import NewVietnameseSong from './NewVietnameseSong'
-// import PlaylistSuggestion from './PlaylistSuggestion'
-// import SongRanking from './SongRanking'
-// import WeeklyAlbumMVRanking from './WeeklyAlbumMVRanking'
-// import WeeklySongRanking from './WeeklySongRanking'
+import CooperationPartner from "./CooperationPartner";
+import HotAlbum from "./HotAlbum";
+import HotArtist from "./HotArtist";
+import HotTopic from "./HotTopic";
+import HotVideo from "./HotVideo";
+import MusicForYou from "./MusicForYou";
+import MusicNews from "./MusicNews";
+import NewVietnameseSong from "./NewVietnameseSong";
+import PlaylistSuggestion from "./PlaylistSuggestion";
+import SongRanking from "./SongRanking";
+import WeeklyAlbumMVRanking from "./WeeklyAlbumMVRanking";
+import WeeklySongRanking from "./WeeklySongRanking";
 //others
 import "./style.scss";
 
@@ -24,7 +24,21 @@ const HomePage = () => (
     <Header />
     <Navbar />
     <div className="content-body-wrapper">
-      <div className="content-body-wrapper-inner">BCBC</div>
+      <div className="content-body-wrapper-inner">
+        <SongRanking />
+        <MusicForYou />
+        <HotTopic />
+        <WeeklySongRanking />
+        <PlaylistSuggestion />
+        <HotVideo />
+        <WeeklyAlbumMVRanking />
+        <HotAlbum />
+        <WeeklyAlbumMVRanking />
+        <MusicNews />
+        <NewVietnameseSong />
+        <HotArtist />
+        <CooperationPartner />
+      </div>
     </div>
     <Footer />
   </div>

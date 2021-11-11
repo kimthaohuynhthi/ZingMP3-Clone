c2ec13b5d781936a45695666ab787ea66d81520f
kimthaohuynhthi <kimthao.huynhthi@m.citynow.vn>
2021-11-11 17:46:41 +0700

    add image backround, image banner, content

--- src/HomePage/MusicForYou/index.jsx
+++ src/HomePage/MusicForYou/index.jsx
@@ -1,9 +1,31 @@
 //libs
 import React from "react";
+//image
+import background from "../../assets/MusicForYou/background.png";
+import image from "../../assets/MusicForYou/musicforyou.jpeg";
 //others
 import "./style.scss";
 
 const MusicForYou = () => (
-  <div className="music-for-you-wrapper">Music For You</div>
+  <div className="music-for-you-wrapper">
+    <div className="music-for-you-wrapper-inner">
+      <div className="background-image">
+        <img alt="background" src={background} height="130px" />
+      </div>
+      <div className="music-for-you">
+        <a href="/">
+          <img src={image} alt="music-for-you" width="90px" height="90px" />
+        </a>
+      </div>
+      <div className="content-music-for-you">
+        <a href="/" className="title">
+          <b>Dành Riêng Cho Bạn</b>
+          <p>
+            Nghe những ca khúc yêu thích và khám phá âm nhạc dành riêng cho bạn.
+          </p>
+        </a>
+      </div>
+    </div>
+  </div>
 );
 export default MusicForYou;

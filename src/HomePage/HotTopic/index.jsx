3703fb5413c295c780caff44af6c175bfe26d0f7
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-15 12:29:25 +0700

    divide component

--- src/HomePage/HotTopic/index.jsx
+++ src/HomePage/HotTopic/index.jsx
@@ -1,24 +1,16 @@
 // libs
 import React from "react";
-import { RightOutlined } from "@ant-design/icons";
-// image
-import background from "../../assets/MusicForYou/background-hottopic.jpg";
+// components
+import Title from "./mains/Title";
+import SeeMore from "./mains/SeeMore";
 // others
 import "./style.scss";
 
 const HotTopic = () => (
   <div className="hot-topic-wrapper">
     <div className="hot-topic-wrapper-inner">
-      <p className="title">CHỦ ĐỀ HOT</p>
-      <a href="/" title="Hôm nay nghe gì?">
-        <img src={background} alt="topic" />
-      </a>
-      <div className="other">
-        <a href="/">
-          Xem thêm Chủ Đề khác
-          <RightOutlined className="icon" />
-        </a>
-      </div>
+      <Title />
+      <SeeMore />
     </div>
   </div>
 );
--- src/HomePage/MusicForYou/index.jsx
+++ src/HomePage/MusicForYou/index.jsx
@@ -1,33 +1,18 @@
 // libs
 import React from "react";
-// icons
-import { PlayCircleOutlined } from "@ant-design/icons";
-// image
-import background from "../../assets/MusicForYou/background.png";
-import image from "../../assets/MusicForYou/musicforyou.jpeg";
+// components
+import BackgroundImage from "./mains/BackgroundImage";
+import Banner from "./mains/Banner";
+import Content from "./mains/Content";
 // others
 import "./style.scss";
 
 const MusicForYou = () => (
   <div className="music-for-you-wrapper">
     <div className="music-for-you-wrapper-inner">
-      <div className="background-image">
-        <img alt="background" src={background} />
-      </div>
-      <div className="banner" title="Playlist của bạn">
-        <a href="/">
-          <img src={image} alt="music-for-you" />
-        </a>
-        <PlayCircleOutlined className="play-icon" />
-      </div>
-      <div className="content-music-for-you">
-        <a href="/" className="title">
-          <b>Dành Riêng Cho Bạn</b>
-          <p>
-            Nghe những ca khúc yêu thích và khám phá âm nhạc dành riêng cho bạn.
-          </p>
-        </a>
-      </div>
+      <BackgroundImage />
+      <Banner />
+      <Content />
     </div>
   </div>
 );

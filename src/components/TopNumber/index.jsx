92449d51766c2e449c1b0a583de4daea347b1e1f
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-15 12:04:08 +0700

    add rule and topnumber component

--- src/components/TopNumber/index.jsx
+++ src/components/TopNumber/index.jsx
@@ -1,7 +1,28 @@
 // libs
-import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
 // others
 import "./style.scss";
 
-const ThumbnailSong = () => <div className="thumbnail-song-wrapper">ABC</div>;
-export default ThumbnailSong;
+const TopNumber = ({ topNumber, numberTitle, fontSize }) => (
+  <div
+    className={classNames(
+      "top-number-wrapper",
+      "top-number-inner",
+      { "top-1-song-ranking": topNumber === "01" },
+      { "top-2-song-ranking": topNumber === "02" },
+      { "top-3-song-ranking": topNumber === "03" },
+      { "others-song-ranking": topNumber === "others" }
+    )}
+    style={{ fontSize }}
+  >
+    {numberTitle}
+  </div>
+);
+TopNumber.propTypes = {
+  topNumber: PropTypes.string,
+  numberTitle: PropTypes.string,
+  fontSize: PropTypes.string,
+};
+
+export default TopNumber;

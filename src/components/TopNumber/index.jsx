17160b2199337f80fe853a228848fe3efd6b9e2a
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-15 10:52:06 +0700

    install classnames

---
+++ src/components/TopNumber/index.jsx
@@ -0,0 +1,7 @@
+// libs
+import React from "react";
+// others
+import "./style.scss";
+
+const ThumbnailSong = () => <div className="thumbnail-song-wrapper">ABC</div>;
+export default ThumbnailSong;

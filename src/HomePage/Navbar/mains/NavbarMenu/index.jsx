47e4361b59139d337c70e2dbb0c135c1524395d6
kimthaohuynhthi <93518511+kimthaohuynhthi@users.noreply.github.com>
2021-11-16 19:03:19 +0700

    [HomePage]Header & Navbar (#2)
    * add folder assets
    
    * header homepage, unedited search bar
    
    * fix font-weight
    
    * layout navbar
    
    * delete form
    
    * fix border menuitems
    
    * add tooltip
    
    * component headersearch, add datasources
    
    * component header menu
    
    * component navbar menu
    
    * fix organize css
    
    * component functional menu

---
+++ src/HomePage/Navbar/mains/NavbarMenu/index.jsx
@@ -0,0 +1,27 @@
+// libs
+import React from "react";
+import { HomeFilled } from "@ant-design/icons";
+// dataSources
+import menuNavbar from "../../../../dataSources/Navbar";
+// others
+import "./style.scss";
+
+const NavbarMenu = () => (
+  <div className="navbar-menu-wrapper">
+    <ul>
+      <li>
+        <a href="/" className="home-icon" title="Zing MP3">
+          <HomeFilled />
+        </a>
+      </li>
+      {menuNavbar.map((menuItem) => (
+        <li>
+          <a href="/" title={menuItem.title}>
+            {menuItem.label}
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+export default NavbarMenu;

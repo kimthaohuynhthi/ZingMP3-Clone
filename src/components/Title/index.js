2976f45a9c7f46a02c692447b3078324dfbf6d5a
kimthaohuynhthi <93518511+kimthaohuynhthi@users.noreply.github.com>
2021-11-16 19:53:54 +0700

    [HomePage] Hot Video (#6)
    * add component video card, list video
    
    * add image
    
    * add word-wrap
    
    * fix word-wrap singername
    
    * fix organization of css
    
    Co-authored-by: hellanngel108 <18520155@gm.uit.edu.vn>
    Co-authored-by: Truong Tan Thuan <t-truong@usen-next.jp>

---
+++ src/components/Title/index.js
@@ -0,0 +1,21 @@
+// libs
+import PropTypes from "prop-types";
+import { RightOutlined } from "@ant-design/icons";
+// others
+import "./style.scss";
+
+const Title = ({ titleName, fontSize }) => (
+  <div className="title-wrapper">
+    <h3 style={{ fontSize }}>
+      <a href="/">
+        {titleName}
+        <RightOutlined />
+      </a>
+    </h3>
+  </div>
+);
+Title.propTypes = {
+  titleName: PropTypes.string,
+  fontSize: PropTypes.string,
+};
+export default Title;

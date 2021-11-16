8860fe17c06018da3a3b79208df32de75347c20b
Truong Tan Thuan <t-truong@usen-next.jp>
2021-11-16 20:05:53 +0700

    Merge branch 'dev' of https://github.com/kimthaohuynhthi/ZingMP3-Clone into feature/kimthaohuynhthi/homepage/layout-weekly-song-ranking

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,15 +1,19 @@
 // libs
 import PropTypes from "prop-types";
-import { RightOutlined } from "@ant-design/icons";
+import classNames from "classnames";
+import { RightOutlined, PlayCircleTwoTone } from "@ant-design/icons";
 // others
 import "./style.scss";
 
-const Title = ({ titleName, fontSize }) => (
+const Title = ({ titleName, fontSize, isShow }) => (
   <div className="title-wrapper">
     <h3 style={{ fontSize }}>
       <a href="/">
         {titleName}
         <RightOutlined />
+      </a>
+      <a href="/" className={classNames("play-icon", { show: isShow })}>
+        <PlayCircleTwoTone twoToneColor="darkgray" />
       </a>
     </h3>
   </div>
@@ -17,5 +21,6 @@ const Title = ({ titleName, fontSize }) => (
 Title.propTypes = {
   titleName: PropTypes.string,
   fontSize: PropTypes.string,
+  isShow: PropTypes.string,
 };
 export default Title;

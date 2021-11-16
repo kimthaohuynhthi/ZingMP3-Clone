9b499d8a4b878ee1b640ff300ca03d82eac62e57
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-16 17:35:17 +0700

    add menuzingchart

---
+++ src/dataSources/MenuZingChart/index.js
@@ -0,0 +1,15 @@
+const menuZingChart = [
+  {
+    label: "Việt Nam",
+    title: "BXH Bài hát Việt Nam",
+  },
+  {
+    label: "US-UK",
+    title: "BXH Bài hát US-UK",
+  },
+  {
+    label: "K-Pop",
+    title: "BXH Bài hát KPop",
+  },
+];
+export default menuZingChart;

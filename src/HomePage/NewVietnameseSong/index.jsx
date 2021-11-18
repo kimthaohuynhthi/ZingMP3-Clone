87039845984a1b4c687a907ce4b853b706d05b1b
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-19 01:02:30 +0700

    add image

--- src/HomePage/NewVietnameseSong/index.jsx
+++ src/HomePage/NewVietnameseSong/index.jsx
@@ -1,9 +1,13 @@
-//libs
+// libs
 import React from "react";
-//others
+// components
+import Title from "../../components/Title";
+// others
 import "./style.scss";
 
 const NewVietnameseSong = () => (
-  <div className="new-song-wrapper">NewVietnameseSong</div>
+  <div className="new-song-wrapper">
+    <Title titleName="NHẠC VIỆT MỚI" fontSize="24px" />
+  </div>
 );
 export default NewVietnameseSong;

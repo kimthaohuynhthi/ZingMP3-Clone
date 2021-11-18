72821a44cd2cb49a792c76f96d0c31725060ff86
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-19 01:09:05 +0700

    add mọk data

---
+++ src/mocks/VPop.js
@@ -0,0 +1,75 @@
+// imgs
+import song1 from "../assets/NewVietnamsesSong/song1.jpg";
+import song2 from "../assets/NewVietnamsesSong/song2.jpg";
+import song3 from "../assets/NewVietnamsesSong/song3.jpg";
+import song4 from "../assets/NewVietnamsesSong/song4.jpg";
+import song5 from "../assets/NewVietnamsesSong/song5.jpg";
+import song6 from "../assets/NewVietnamsesSong/song6.jpg";
+import song7 from "../assets/NewVietnamsesSong/song7.jpg";
+import song8 from "../assets/NewVietnamsesSong/song8.jpg";
+import song9 from "../assets/NewVietnamsesSong/song9.jpg";
+import song10 from "../assets/NewVietnamsesSong/song10.jpg";
+
+const datasong = [
+  {
+    id: "1",
+    name: "Kiêu Ngạo",
+    singer: "Huy Vạc",
+    img: song1,
+  },
+  {
+    id: "2",
+    name: "Cứ Nói Yêu Lần Này",
+    singer: "Lil Z,CUKAK",
+    img: song2,
+  },
+  {
+    id: "3",
+    name: "Mai Em Về Bên Ấy",
+    singer: "Ngọc Hân",
+    img: song3,
+  },
+  {
+    id: "4",
+    name: "Tôi Không Cần Trường Sinh Bất Lão",
+    singer: "Tào Lữ Phụ",
+    img: song4,
+  },
+  {
+    id: "5",
+    name: "Như Áng Mây Trời",
+    singer: "T-Passion",
+    img: song5,
+  },
+  {
+    id: "6",
+    name: "Câu Chuyện Cười",
+    singer: "DuUyen",
+    img: song6,
+  },
+  {
+    id: "7",
+    name: "Tình Yêu Chân Thành",
+    singer: "Trương Thảo Nhi",
+    img: song7,
+  },
+  {
+    id: "8",
+    name: "Like the first moment i met you",
+    singer: "Kim Feel",
+    img: song8,
+  },
+  {
+    id: "9",
+    name: "Yêu Là Cưới",
+    singer: "Phát Hồ, X2X",
+    img: song9,
+  },
+  {
+    id: "10",
+    name: "Hai Đứa Này Mệt Ghê",
+    singer: "Long Nón Lá,The 199X",
+    img: song10,
+  },
+];
+export default datasong;

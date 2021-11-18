8683738a79b39042fbf6109bd0609970eec813d9
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-18 10:51:51 +0700

    fix classname

--- src/components/TopNumber/index.jsx
+++ src/components/TopNumber/index.jsx
@@ -8,13 +8,12 @@ const TopNumber = ({ topNumber, numberTitle, fontSize }) => (
   <div
     className={classNames(
       "top-number-wrapper",
-      "top-number-inner",
+      "top-number-wrapper-inner",
       { "top-1-song-ranking": topNumber === "01" },
       { "top-2-song-ranking": topNumber === "02" },
       { "top-3-song-ranking": topNumber === "03" },
-      { "top-1-zing-chart": topNumber === "top-1-zing-chart" },
-      { "zing-chart": topNumber === "zing-chart" },
-      { "others-song-ranking": topNumber === "others" }
+      { "others-song-ranking": topNumber === "others" },
+      { "top-1-zing-chart": topNumber === "top-1-zing-chart" }
     )}
     style={{ fontSize }}
   >

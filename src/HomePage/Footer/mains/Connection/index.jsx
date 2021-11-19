82d535abe54bda61a20970199e99ef93602a1351
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-19 17:05:54 +0700

    add datasource menu footer

--- src/HomePage/Footer/mains/Connection/index.jsx
+++ src/HomePage/Footer/mains/Connection/index.jsx
@@ -3,5 +3,10 @@ import React from "react";
 //others
 import "./style.scss";
 
-const Connection = () => <div className="connection-wrapper">FOOTER</div>;
+const Connection = () => (
+  <div className="connection-wrapper">
+    <div className="connection-wrapper">FOOTER</div>;
+  </div>
+);
+
 export default Connection;
---
+++ src/dataSources/Footer/index.js
@@ -0,0 +1,44 @@
+const menuFooter = [
+  {
+    id: 1,
+    name: "Giới thiệu",
+  },
+  {
+    id: 2,
+    name: "Điều khoản",
+  },
+  {
+    id: 3,
+    name: "Quảng cáo",
+  },
+  {
+    id: 4,
+    name: "FAQs",
+  },
+  {
+    id: 5,
+    name: "Copyright",
+  },
+  {
+    id: 6,
+    name: "Zing MP3 VIP",
+  },
+  {
+    id: 7,
+    name: "Ứng dụng",
+  },
+  {
+    id: 8,
+    name: "Góp ý",
+  },
+  {
+    id: 9,
+    name: "APIs",
+  },
+  {
+    id: 10,
+    name: "Liên hệ",
+  },
+];
+
+export { menuFooter };

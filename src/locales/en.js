2758e93ceedc15aa36d3c48e1ae43ce443c59bfd
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-19 11:24:21 +0700

    add locales

---
+++ src/locales/en.js
@@ -0,0 +1,14 @@
+const en = {
+  nhaccanhan: "My song",
+  zingchart: "#zing chart",
+  top100: "Top 100",
+  chude: "Topic",
+  video: "Video",
+  album: "Album",
+  nghesi: "Singer",
+  vip: "VIP",
+  tatquangcao: "Turn off ads",
+  vi: "Tiếng Việt",
+  en: "English",
+};
+export default en;
---
+++ src/locales/vi.js
@@ -0,0 +1,14 @@
+const vi = {
+  nhaccanhan: "Nhạc Của Tôi",
+  zingchart: "#zing chart",
+  top100: "Top 100",
+  chude: "Chủ Đề",
+  video: "Video",
+  album: "Album",
+  nghesi: "Nghệ Sĩ",
+  vip: "VIP",
+  tatquangcao: "Turn off ads",
+  vi: "Tiếng Việt",
+  en: "English",
+};
+export default vi;

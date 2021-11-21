1946ff7bfdbc8fc448ae4a81fdf005bf3e4c8952
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-22 00:51:41 +0700

    add imgae

--- src/HomePage/MusicNews/index.jsx
+++ src/HomePage/MusicNews/index.jsx
@@ -1,7 +1,13 @@
 //libs
 import React from "react";
+// components
+import Title from "../../components/Title";
 //others
 import "./style.scss";
 
-const MusicNews = () => <div className="music-news-wrapper">Music News</div>;
+const MusicNews = () => (
+  <div className="music-news-wrapper">
+    <Title titleName="TIN TỨC ÂM NHẠC" fontSize="20px" />
+  </div>
+);
 export default MusicNews;

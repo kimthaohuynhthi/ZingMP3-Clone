975f8897b4cb1921fb6e82ad6a3bf9b4ffa70ee5
kimthaohuynhthi <93518511+kimthaohuynhthi@users.noreply.github.com>
2021-11-21 12:57:29 +0700

    [HomePage]Hot artist (#11)
    * add image
    
    * add mock data
    
    * add image card
    
    Co-authored-by: hellanngel108 <18520155@gm.uit.edu.vn>

---
+++ src/mocks/ListArtist.js
@@ -0,0 +1,53 @@
+// images
+import singer1 from "../assets/Artist/singer1.jpg";
+import singer2 from "../assets/Artist/singer2.jpg";
+import singer3 from "../assets/Artist/singer3.jpg";
+import singer4 from "../assets/Artist/singer4.jpg";
+import singer5 from "../assets/Artist/singer5.jpg";
+import singer6 from "../assets/Artist/singer6.jpg";
+import singer7 from "../assets/Artist/singer7.jpg";
+import singer8 from "../assets/Artist/singer8.jpg";
+
+const listArtist = [
+  {
+    id: "01",
+    name: "Võ Kiều Vân",
+    img: singer1,
+  },
+  {
+    id: "02",
+    name: "Đan Nguyên",
+    img: singer2,
+  },
+  {
+    id: "03",
+    name: "Thảo Wendy",
+    img: singer3,
+  },
+  {
+    id: "04",
+    name: "Chi Dân",
+    img: singer4,
+  },
+  {
+    id: "05",
+    name: "Nhật Nguyễn",
+    img: singer5,
+  },
+  {
+    id: "06",
+    name: "Erik",
+    img: singer6,
+  },
+  {
+    id: "07",
+    name: "K-ICM",
+    img: singer7,
+  },
+  {
+    id: "08",
+    name: "Lê Bảo Bình",
+    img: singer8,
+  },
+];
+export default listArtist;

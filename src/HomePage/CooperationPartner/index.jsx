8d08e992e2318043e1a034ac2c35366d5a930560
kimthaohuynhthi <93518511+kimthaohuynhthi@users.noreply.github.com>
2021-11-21 13:03:16 +0700

    [HomePage] Cooperation partner (#13)
    * git add image and dataSource
    
    * add layout cooperation partner
    
    Co-authored-by: hellanngel108 <18520155@gm.uit.edu.vn>

--- src/HomePage/CooperationPartner/index.jsx
+++ src/HomePage/CooperationPartner/index.jsx
@@ -1,9 +1,20 @@
 //libs
 import React from "react";
+// dataSource
+import listPartner from "../../dataSources/Partner";
 //others
 import "./style.scss";
 
 const CooperationPartner = () => (
-  <div className="cooperation-partner-wrapper">COOPERATION PARTNER</div>
+  <div className="cooperation-partner-wrapper">
+    <div className="partner-title">ĐỐI TÁC</div>
+    <ul className="partner-list">
+      {listPartner.map((partner) => (
+        <li key={partner.id}>
+          <img src={partner.image} alt="partner" />
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 export default CooperationPartner;
---
+++ src/dataSources/Partner/index.js
@@ -0,0 +1,55 @@
+import benthanhPartner from "../../assets/Partner/partner1.jpeg";
+import universalPartner from "../../assets/Partner/partner2.png";
+import sonymusicPartner from "../../assets/Partner/partner3.png";
+import thuyngaPartner from "../../assets/Partner/partner4.jpg";
+import vcpmcPartner from "../../assets/Partner/partner5.jpg";
+import rianPartner from "../../assets/Partner/partner6.png";
+import kimloiPartner from "../../assets/Partner/partner7.jpg";
+import phuongnamPartner from "../../assets/Partner/partner8.jpg";
+import rangdongPartner from "../../assets/Partner/partner9.jpg";
+import vafacoPartner from "../../assets/Partner/partner10.jpg";
+
+const listPartner = [
+  {
+    id: 1,
+    image: universalPartner,
+  },
+  {
+    image: sonymusicPartner,
+    id: 2,
+  },
+  {
+    image: thuyngaPartner,
+    id: 3,
+  },
+  {
+    image: vcpmcPartner,
+    id: 4,
+  },
+  {
+    image: rianPartner,
+    id: 5,
+  },
+  {
+    image: kimloiPartner,
+    id: 6,
+  },
+  {
+    image: phuongnamPartner,
+    id: 7,
+  },
+  {
+    image: rangdongPartner,
+    id: 8,
+  },
+  {
+    image: vafacoPartner,
+    id: 9,
+  },
+  {
+    image: benthanhPartner,
+    id: 10,
+  },
+];
+
+export default listPartner;

7053406e7ef7558537ba91e18c1631231f0a68e6
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-22 00:54:44 +0700

    add mock data

---
+++ src/mocks/ListNews.js
@@ -0,0 +1,35 @@
+// images
+import new1 from "../assets/MusicNews/news1.webp";
+import new2 from "../assets/MusicNews/news2.webp";
+import new3 from "../assets/MusicNews/news3.webp";
+import new4 from "../assets/MusicNews/news4.webp";
+import new5 from "../assets/MusicNews/news5.webp";
+
+const news = [
+  {
+    id: "01",
+    name: "Adele ra mắt MV nói về hôn nhân tan vỡ",
+    img: new1,
+  },
+  {
+    id: "02",
+    name: "Nhóm nhạc nữ đang thống trị Kpop",
+    img: new2,
+  },
+  {
+    id: "03",
+    name: "Ca sĩ Kpop giờ đây không nhất thiết phải hát tiếng Hàn",
+    img: new3,
+  },
+  {
+    id: "04",
+    name: "Adele khác biệt từ những điều giản dị",
+    img: new4,
+  },
+  {
+    id: "05",
+    name: "Nhạc đám cưới khuấy đảo Vpop",
+    img: new5,
+  },
+];
+export default news;

c03b18b79599729babe04a3dcc9ed5ca5fc9e3d5
kimthaohuynhthi <93518511+kimthaohuynhthi@users.noreply.github.com>
2021-11-22 14:27:03 +0700

    [HomePage] Music News (#16)
    * add imgae
    
    * add mock data
    
    * add list news
    
    * fix css newvietnamesesong
    
    Co-authored-by: hellanngel108 <18520155@gm.uit.edu.vn>

--- src/HomePage/MusicNews/index.jsx
+++ src/HomePage/MusicNews/index.jsx
@@ -1,7 +1,19 @@
-//libs
+// libs
 import React from "react";
-//others
+// components
+import Title from "../../components/Title";
+import ListNews from "./ListNews";
+// image
+import bg from "../../assets/MusicNews/bg.webp";
+// others
 import "./style.scss";
 
-const MusicNews = () => <div className="music-news-wrapper">Music News</div>;
+const MusicNews = () => (
+  <div className="music-news-wrapper">
+    <Title titleName="TIN TỨC ÂM NHẠC" fontSize="20px" />
+    <img src={bg} alt="news" />
+    <p>Lý do năm 2022 sẽ là năm của BTS</p>
+    <ListNews />
+  </div>
+);
 export default MusicNews;

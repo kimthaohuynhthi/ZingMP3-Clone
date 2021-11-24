eadcd2234823b2e14522665831684db312023f56
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-24 11:55:51 +0700

    fetch data song in top

--- src/HomePage/SongRanking/mains/TopSong/index.jsx
+++ src/HomePage/SongRanking/mains/TopSong/index.jsx
@@ -1,26 +1,42 @@
 // libs
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+// components
 import SongCard from "../../components/SongCard";
+//actions
+import { fetchSongInTop } from "../../../../redux/actions/fetchData";
 // mocks
 import songInTop from "../../../../mocks/TopSong";
 // others
 import "./style.scss";
 
-const TopSong = () => (
-  <div className="top-song-wrapper">
-    {songInTop.map((song) => (
-      <ul className="top-song-wrapper-inner" key={song.id}>
-        <li>
-          <SongCard
-            topNumber={song.rank}
-            numberTitle={song.number}
-            image={song.img}
-            songName={song.name}
-            singerName={song.singer}
-          />
-        </li>
-      </ul>
-    ))}
-  </div>
-);
+const TopSong = () => {
+  const dispatch = useDispatch();
+  const stateSongInTop = useSelector(
+    (state) => state.dataReducer.dataSongInTop
+  );
+
+  useEffect(() => {
+    dispatch(fetchSongInTop(songInTop));
+  }, []);
+
+  return (
+    <div className="top-song-wrapper">
+      {stateSongInTop.map((song) => (
+        <ul className="top-song-wrapper-inner" key={song.id}>
+          <li>
+            <SongCard
+              topNumber={song.rank}
+              numberTitle={song.number}
+              image={song.img}
+              songName={song.name}
+              singerName={song.singer}
+            />
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
 export default TopSong;

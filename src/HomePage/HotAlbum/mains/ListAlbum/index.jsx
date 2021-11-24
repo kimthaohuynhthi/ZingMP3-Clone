bf48c6b53e0873f52a4ba762203d9b54743f81b6
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-24 11:03:57 +0700

    install redux-thunk, add store, fetch album hot

--- src/HomePage/HotAlbum/mains/ListAlbum/index.jsx
+++ src/HomePage/HotAlbum/mains/ListAlbum/index.jsx
@@ -1,25 +1,37 @@
 // libs
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 // components
 import AlbumCard from "../../components/AlbumCard";
+//actions
+import { fetchAlbumHot } from "../../../../redux/actions/fetchData";
 // mocks
 import listAlbum from "../../../../mocks/ListAlbum";
 // others
 import "./style.scss";
 
-const ListAlbum = () => (
-  <div className="list-album-wrapper">
-    <ul className="list-album-wrapper-inner">
-      {listAlbum.map((album) => (
-        <li key={album.id}>
-          <AlbumCard
-            songName={album.albumName}
-            singerName={album.singerName}
-            image={album.img}
-          />
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const ListAlbum = () => {
+  const dispatch = useDispatch();
+  const stateAlbum = useSelector((state) => state.dataReducer.data);
+
+  useEffect(() => {
+    dispatch(fetchAlbumHot(listAlbum));
+  }, []);
+
+  return (
+    <div className="list-album-wrapper">
+      <ul className="list-album-wrapper-inner">
+        {stateAlbum.map((album) => (
+          <li key={album.id}>
+            <AlbumCard
+              songName={album.albumName}
+              singerName={album.singerName}
+              image={album.img}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default ListAlbum;

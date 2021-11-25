f4213db018415d4c04d9d17649838cc9c0edb30f
hellanngel108 <18520155@gm.uit.edu.vn>
2021-11-25 18:16:01 +0700

    add folder types, add index

--- src/redux/actions/fetchData.js
+++ src/redux/actions/fetchData.js
@@ -1,51 +1,51 @@
-import * as action from "../actionTypes";
+import { actionTypes } from "../types/index";
 
 export const fetchAlbumHot = (data) => ({
-  type: action.FETCH_ALBUM_HOT,
+  type: actionTypes.FETCH_ALBUM_HOT,
   payload: data,
 });
 
 export const fetchVideoHot = (data) => ({
-  type: action.FETCH_VIDEO_HOT,
+  type: actionTypes.FETCH_VIDEO_HOT,
   payload: data,
 });
 
 export const fetchMusicNews = (data) => ({
-  type: action.FETCH_MUSIC_NEWS,
+  type: actionTypes.FETCH_MUSIC_NEWS,
   payload: data,
 });
 
 export const fetchSongInTop = (data) => ({
-  type: action.FETCH_SONG_IN_TOP,
+  type: actionTypes.FETCH_SONG_IN_TOP,
   payload: data,
 });
 
 export const fetchZingChartSong = (data) => ({
-  type: action.FETCH_ZING_CHART_SONG,
+  type: actionTypes.FETCH_ZING_CHART_SONG,
   payload: data,
 });
 
 export const fetchZingChartMV = (data) => ({
-  type: action.FETCH_ZING_CHART_MV,
+  type: actionTypes.FETCH_ZING_CHART_MV,
   payload: data,
 });
 
 export const fetchZingChartAlbum = (data) => ({
-  type: action.FETCH_ZING_CHART_ALBUM,
+  type: actionTypes.FETCH_ZING_CHART_ALBUM,
   payload: data,
 });
 
 export const fetchVietnameseSong = (data) => ({
-  type: action.FETCH_VIETNAMESE_SONG,
+  type: actionTypes.FETCH_VIETNAMESE_SONG,
   payload: data,
 });
 
 export const fetchArtist = (data) => ({
-  type: action.FETCH_HOT_ARTIST,
+  type: actionTypes.FETCH_HOT_ARTIST,
   payload: data,
 });
 
 export const fetchPlaylist = (data) => ({
-  type: action.FETCH_PLAYLIST,
+  type: actionTypes.FETCH_PLAYLIST,
   payload: data,
 });

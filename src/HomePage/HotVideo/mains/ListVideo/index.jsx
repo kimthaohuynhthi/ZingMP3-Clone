b7b2f88fc52e769efd2b6d4c32a9353999ba242e
kimthaohuynhthi <93518511+kimthaohuynhthi@users.noreply.github.com>
2021-11-28 10:28:38 +0700

    [HomePage] Setup Redux Thunk (#18)
    * install redux
    
    * add actionTypes
    
    * add actions
    
    * add dataReducer
    
    * install redux-thunk, add store, fetch album hot
    
    * fetch data video hot
    
    * fetch data music news
    
    * fetch data song in top
    
    * fetch data zing chart song
    
    * fetch data zing chart mv
    
    * fetch data zing chart album
    
    * fetch data vietnamese song
    
    * fetch data playlist
    
    * add folder types, add index
    
    * add loading fetch album
    
    Co-authored-by: hellanngel108 <18520155@gm.uit.edu.vn>
    Co-authored-by: Truong Tan Thuan <t-truong@usen-next.jp>

--- src/HomePage/HotVideo/mains/ListVideo/index.jsx
+++ src/HomePage/HotVideo/mains/ListVideo/index.jsx
@@ -1,25 +1,37 @@
 // libs
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 // components
 import VideoCard from "../../components/VideoCard";
+//actions
+import { fetchVideoHot } from "../../../../redux/actions/fetchData";
 // mocks
 import listVideo from "../../../../mocks/ListVideo";
 // others
 import "./style.scss";
 
-const ListVideo = () => (
-  <div className="list-video-wrapper">
-    <ul className="list-video-wrapper-inner">
-      {listVideo.map((video) => (
-        <li key={video.id}>
-          <VideoCard
-            songName={video.videoName}
-            singerName={video.singerName}
-            image={video.img}
-          />
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+const ListVideo = () => {
+  const dispatch = useDispatch();
+  const stateVideo = useSelector((state) => state.dataReducer.dataVideo);
+
+  useEffect(() => {
+    dispatch(fetchVideoHot(listVideo));
+  }, []);
+
+  return (
+    <div className="list-video-wrapper">
+      <ul className="list-video-wrapper-inner">
+        {stateVideo.map((video) => (
+          <li key={video.id}>
+            <VideoCard
+              songName={video.videoName}
+              singerName={video.singerName}
+              image={video.img}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default ListVideo;
